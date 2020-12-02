d2d9597aef5b1b934d0916932cf92995ebf87845
Quick <577652+markdwags@users.noreply.github.com>
2020-12-02 11:53:25 -0600

    Added a sleep controller to test for long responses

---
+++ SampleWebApiAspNetCore/Controllers/v1/SleepController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using AutoMapper;
+using SampleWebApiAspNetCore.Dtos;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using SampleWebApiAspNetCore.Repositories;
+using System.Collections.Generic;
+using System.Net.Http;
+using SampleWebApiAspNetCore.Entities;
+using SampleWebApiAspNetCore.Models;
+using SampleWebApiAspNetCore.Helpers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace SampleWebApiAspNetCore.v1.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    //[Route("api/[controller]")]
+    public class SleepController : ControllerBase
+    {
+        private IHttpClientFactory _httpClientFactory;
+
+        public SleepController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        [Route("{id:int}", Name = nameof(SleepTime))]
+        public ActionResult SleepTime(int id)
+        {
+            System.Threading.Thread.Sleep(id);
+
+            return Ok();
+        }
+
+       
+    }
+}
